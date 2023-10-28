1833d3573c69af3f5a19150645edfb1147c8fc72
fnalviar <nikka_alviar@outlook.com>
2023-10-28 00:14:14 -0400

    resume updated

--- src/config/resume/experienceConfig.js
+++ src/config/resume/experienceConfig.js
@@ -3,9 +3,9 @@ export const experienceConfig = [
     name: "Scaling",
     title: "Frontend Developer",
     description: [
-      "Spearheaded improvements in user experience, resulting in a 30% increase in user engagement and a 20% reduction in bounce rate, achieved by implementing dynamic route changes",
-      "Successfully integrated OAuth for Calendly using its API, enhancing user interaction, and extending sessions beyond the initial 2-hour OAuth validity, resulting in a 40% increase in user sessions and a 15% boost in authentication efficiency",
-      "Designed and developed a robust backend API with Node.js, resulting in a 25% improvement in system scalability, a 30% increase in overall system flexibility, and a 20% reduction in user request latency through seamless frontend-backend interaction",
+      "Spearheaded initiatives resulting in a 30% increase in user engagement and a 20% reduction in bounce rate by implementing dynamic route changes",
+      "Enhanced user interaction and extended OAuth sessions, leading to a 40% increase in user sessions and a 15% boost in authentication efficiency by integrating OAuth for Calendly using its API",
+      "Designed and developed a scalable backend API with Node.js, improving system scalability by 25%, overall system flexibility by 30%, and reducing user request latency by 20% through seamless frontend-backend interaction",
     ],
     date: "Jan 2023 - Present",
   },
@@ -15,8 +15,8 @@ export const experienceConfig = [
     description: [
       "Collaborated with cross-functional teams to manage 3 agile projects from start to finish",
       "Developed a user-friendly Expense Reimbursement e-commerce website and web application using Java, Angular, JavaScript, and PostgreSQL, resulting in a 15% increase in user engagement",
-      "Ensured high-quality code by conducting comprehensive unit testing with JUnit and Mockito, achieving a minimum passing rate of 70%",
-      "Partnered with a team of 3 developers to deliver high-quality web applications on tight deadlines, adhering to Agile Methodology with bi-weekly sprints, resulting in a 100% success rate in project delivery",
+      "Utilized Git and Maven for version control and build automation, reducing build times by 20% and improving the overall software development process",
+      "Delivered high-quality web applications on tight deadlines, adhering to Agile Methodology with bi-weekly sprints, achieving a 100% success rate in project delivery",
     ],
     date: "Sept 2022 - Dec 2022",
   },
